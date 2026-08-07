0f3eb9fc42eae750e016757e0f830c69d79195af
ivanna-tchinda <ivanna.tchinda@gmail.com>
2026-08-07 18:56:27 +0200

    using numpy for array manipulation

--- 1-Array/ex00/give_bmi.py
+++ 1-Array/ex00/give_bmi.py
@@ -1,31 +1,20 @@
-import numpy
+import numpy as np
 
 def give_bmi(height: list[int | float], weight: list[int | float]) -> list[int | float]:
-    height_pow = [x**2 for x in height]
-    bmi = []
-    for h in range(len(height)):
-        bmi.append(weight[h]/height_pow[h])
-    return bmi
+    """This function returns the bmi according to the weight and height given"""
+
+    if(len(height) != len(weight)):
+        ValueError("Lenghts of height and weight should be equal")
+    try:
+        height_pow = np.array(height)
+        height_pow = np.power(height_pow, 2)
+        bmi = np.array(len(height))
+        bmi = np.divide(weight, height_pow)
+    except Exception as e:
+        print(f"Error : {e}")
+    return bmi.tolist()
 
 
 def apply_limit(bmi: list[int | float], limit: int) -> list[bool]:
-    res = [True if x > limit else False for x in bmi]
-    return res
-
-
-
-#checker les exceptions
-
-def main():
-  """This is the main function"""
-  try:
-    height = [2.71, 1.15]
-    weight = [165.3, 38.4]
-    bmi = give_bmi(height, weight)
-    print(bmi, type(bmi))
-    print(apply_limit(bmi, 26))
-  except AssertionError as error:
-    print("AssertionError:", error)
-
-if __name__ == "__main__":
-    main()
+    """This function tells wether or not a bmi is above a limit or not"""
+    return (np.array(bmi) > limit).tolist()
